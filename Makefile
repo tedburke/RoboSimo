e5a22a5409acf244dedc78f1265763712c0dd5a6
tedburke <teddy.burke@gmail.com>
2009-09-26 19:08:58 +0100

    Started putting together the GLUT structure for the program.

--- Makefile
+++ Makefile
@@ -1,16 +1,10 @@
-all: robosimo.exe gluttest.exe opengltest.exe
-
-opengltest.exe: opengltest.c
-	gcc -Wall -s -O2 -mwindows -o opengltest.exe opengltest.c -lopengl32 -lglu32
-
-gluttest.exe: gluttest.o
-	gcc -o gluttest.exe gluttest.o glut32.lib -lopengl32
-
-gluttest.o: gluttest.c
-	gcc -c gluttest.c
+all: robosimo.exe
 
 robosimo.exe: main.o
-	gcc -o robosimo.exe main.o `pkg-config --libs gtk+-2.0`
+	gcc -o robosimo.exe main.o glut32.lib -lopengl32
 
 main.o: main.c
-	gcc -c main.c `pkg-config --cflags gtk+-2.0`
+	gcc -c main.c
+
+#opengltest.exe: opengltest.c
+#	gcc -Wall -s -O2 -mwindows -o opengltest.exe opengltest.c -lopengl32 -lglu32
--- main.c
+++ main.c
@@ -1,40 +1,91 @@
+//
+// RoboSimo - The RoboSumo simulator - Ted Burke - 26-9-2009
+//
+// Programs written in C can access this simulator over
+// the network and control a virtual robot within the
+// simulated environment.
+//
+//
+
 #define _STDCALL_SUPPORTED
 #define _M_IX86
 #include "GL/glut.h"
 
 #include <stdio.h>
 
+void update(void);
 void display(void);
 void keyboard(unsigned char key, int x, int y);
 
+// Window handle
 static int window;
+
+// Global flags
+int program_exiting = 0; // to be set to 1 when program is exiting
+int fullscreen = 0; // to be set to 1 for fullscreen mode
+
+// For counting calls to display function during debugging
+static int counter = 0;
 
 int main(int argc, char **argv)
 {
 	// Start GLUT
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+
 	glutInitWindowSize(640,400);
 	glutInitWindowPosition(100,100);
-	
-	// glutFullScreen();
-	
 	window = glutCreateWindow("RoboSimo");
+
+	// Go fullscreen if flag is set
+	if (fullscreen) glutFullScreen();
 	
+	// Register callback functions
+	glutIdleFunc(update);
 	glutKeyboardFunc(keyboard);
 	glutDisplayFunc(display);
 	
-	glClearColor(0, 0, 0, 0);
+	// Set the background colour to green
+	glClearColor(0, 1, 0, 0);
 	
+	// Enter the main event loop
 	glutMainLoop();
-	
-	printf("Exited GLUT main loop - program closing\n");
+
+	// The program will actually never reach this point
+	// because glutMainLoop() never returns before the
+	// program exits.
 	return 0;
+}
+
+void update()
+{
+	// Check if program should exit
+	if (program_exiting)
+	{
+		// Clean up, then exit
+		glutDestroyWindow(window);
+		exit(0);
+	}
+	
+	// Update scene
+	
+	// Request redrawing of scene
+	glutPostRedisplay();
+}
+
+void reshape(int width, int height)
+{
+	// Set viewport to new window size
+	glViewport(0,0,width,height);
+	
 }
 
 void display()
 {
-	// do  a clearscreen
+	// Print a message to the console
+	printf("In display function, counter = %d\n", counter++);
+	
+	// Clear the background
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	// draw something
@@ -53,10 +104,17 @@ void display()
 	// glutSolidCone(0.5,0.5,100,100);
 	// glutWireCube(0.5);
 	// glutSolidCube(0.5);
+	
+	// Trigger the next draw straight away, so that scene
+	// will keep updating.
+	
+	// Swap back buffer to screen
+	glutSwapBuffers();
 }
 
 void keyboard(unsigned char key, int x, int y)
 {
 	printf("Pressed key %c on coordinates %d,%d\n", key, x, y);
-	if (key == 'q') glutDestroyWindow(window);
+	
+	if (key == 'q') program_exiting = 1; // Set exiting flag
 }
